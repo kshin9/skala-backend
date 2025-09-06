367c93f7b3a99040abf9220601694865a1305f6b
Kyubean Shin <100172941+kshin9@users.noreply.github.com>
2025-09-06 20:37:39 +0900

    Add files via upload

---
+++ src/test/java/com/skala/trading/TradingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skala.trading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
